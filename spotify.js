122e48ee63383203af81053f5432e31e310b67cd
Clement Denis <le.mikmac@gmail.com>
2019-03-04 02:32:07 +0000

    refactor spotify

--- spotify.js
+++ spotify.js
@@ -14,6 +14,7 @@ const conf = (() => {
   catch (err) { return {} }
 })()
 
+// Object.fromEntries: [['a', 1], ['b', 2]] => { a: 1, b: 2 }
 const fromEntries = iter =>
   Object.assign(...[...iter].map(([ k, v ]) => ({ [k]: v })))
 
@@ -21,10 +22,10 @@ const updateConf = newConf =>
   writeFile(confPath, JSON.stringify({ ...conf, ...newConf }, null, 2))
 
 const knownErrors = {
-  NO_ACTIVE_DEVICE: 'No active devices found, you need to open spotify or restart it.',
+  RATE_LIMITED: 'The user is rate limited due to too frequent track play, also known as cat-on-the-keyboard spamming.',
   NO_NEXT_TRACK: 'No next track found',
   NO_PREV_TRACK: 'No previous track found',
-  RATE_LIMITED: 'The user is rate limited due to too frequent track play, also known as cat-on-the-keyboard spamming.',
+  NO_ACTIVE_DEVICE: 'No active devices found, you need to open spotify or restart it.',
   PREMIUM_REQUIRED: 'Spotify premium is required for this',
 }
 
@@ -33,12 +34,9 @@ const haltAndCatchFire = err => {
   process.exit(1)
 }
 
-const port = 7584
-const client_id = conf.id
-const client_secret = conf.secret
+const port = conf.port || 7584
 const redirect_uri = `http://localhost:${port}`
 const param = String(process.argv[2])
-const tryJSON = data => { try { return JSON.parse(data) } catch (err) { return data } }
 const getBody = async stream => {
   const data = []
 
@@ -46,62 +44,51 @@ const getBody = async stream => {
     data.push(chunk.toString('utf8'))
   }
 
-  return tryJSON(data.join(''))
+  try {
+    return JSON.parse(data.join(''))
+  } catch (err) {
+    return data
+  }
 }
 
-const appToken = `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`
-const accountURL = 'https://accounts.spotify.com/api'
+const toBase64 = str => Buffer.from(str).toString('base64')
+const appToken = `Basic ${toBase64(`${conf.id}:${conf.secret}`)}`
 const playerURL = 'https://api.spotify.com/v1/me/player'
+const accountURL = 'https://accounts.spotify.com'
 const contentType = 'application/x-www-form-urlencoded'
+
 const spotify = (method, url, { token, body }) => new Promise((resolve, reject) => {
   const headers = { Authorization: token, 'content-type': contentType }
   request(url, { method, headers }, async response => {
-    if (response.statusCode !== 200) {
-      if (response.statusCode === 204) return resolve(null)
-      return reject(Object.assign(Error(response.statusMessage), {
-        url,
-        method,
-        headers,
-        code: response.statusCode,
-        requestBody: body,
-        ...(await getBody(response)).error
-      }))
-    }
+    if (response.statusCode === 200) return resolve(getBody(response))
+    if (response.statusCode === 204) return resolve(null)
 
-    try { resolve(getBody(response)) }
-    catch (err) { reject(err) }
+    reject(Object.assign(Error(response.statusMessage), {
+      url,
+      method,
+      headers,
+      code: response.statusCode,
+      requestBody: body,
+      ...(await getBody(response)).error
+    }))
   }).on('error', reject)
     .end(body && new URLSearchParams(body).toString())
 })
 
-spotify.post = (path, options) => spotify('POST', path, options)
-spotify.put = (path, options) => spotify('PUT', path, options)
-spotify.get = (path, { body, token } = {}) => spotify(
-  'GET',
-  body ? `${path}?${new URLSearchParams(body)}` : path,
-  { token },
-)
-
-const actions = {
-  play: token => spotify.put(`${playerURL}/play`, { token }),
-  next: token => spotify.post(`${playerURL}/next`, { token }),
-  pause: token => spotify.put(`${playerURL}/pause`, { token }),
-  previous: token => spotify.post(`${playerURL}/previous`, { token }),
-  default: async token => {
-    const pl = await spotify.get(`${playerURL}/currently-playing`, { token })
+const player = {
+  play: token => spotify('PUT', `${playerURL}/play`, { token }),
+  next: token => spotify('POST', `${playerURL}/next`, { token }),
+  pause: token => spotify('PUT', `${playerURL}/pause`, { token }),
+  previous: token => spotify('POST', `${playerURL}/previous`, { token }),
+  toggle: async token => {
+    const pl = await spotify('GET', `${playerURL}/currently-playing`, { token })
     const nextState = pl && pl.is_playing ? 'pause' : 'play'
-    await spotify.put(`${playerURL}/${nextState}`, { token })
+    return spotify('PUT', `${playerURL}/${nextState}`, { token })
   }
 }
 
-const execParam = token => (actions[param] || actions.default)(`Bearer ${token}`)
-
-const similarSpelling = fromEntries(Object.entries({
-  logout: [ 'sign-out', 'signout' ],
-  previous: [ 'previou', 'pervious', 'prev', 'precedent' ],
-}).flatMap(([ param, alts ]) => alts.map(alt => [ alt, param ])))
-
-const expiredAt = conf.timestamp + conf.expires_in * 1000
+const executeSpotifyCommand = token =>
+  (player[param.toLowerCase()] || player.toggle)(`Bearer ${token}`)
 
 const template = text => `
   <div style="font-family:mono;font-size:30;text-align:center;padding-top:5em">
@@ -110,6 +97,18 @@ const template = text => `
     You can close this tab now
   </div>
 `
+
+const similarSpelling = fromEntries(Object.entries({
+  logout: [ 'sign-out', 'signout' ],
+  previous: [ 'previou', 'pervious', 'prev', 'precedent' ],
+}).flatMap(([ param, alts ]) => alts.map(alt => [ alt, param ])))
+
+if (similarSpelling[param]) {
+  console.error(`Unknown param <${param}>, did you mean <${similarSpelling[param]}> ?`)
+  process.exit(1)
+}
+
+const expiredAt = conf.timestamp + conf.expires_in * 1000
 
 if (!conf.id || !conf.secret) {
   console.error(`Incomplete crendentials:
@@ -122,48 +121,49 @@ if (!conf.id || !conf.secret) {
 
   Then edit them here: ${confPath}`)
 
-  updateConf({ id: '', secret: '' })
-    .then(() => process.exit(1))
-    .catch(haltAndCatchFire)
-}
+  updateConf({ id: '', secret: '', port, ...conf })
+    .then(() => process.exit(1), haltAndCatchFire)
 
-if (similarSpelling[param]) {
-  console.error(`Unknown param <${param}>, did you mean <${similarSpelling[param]}> ?`)
-  process.exit(1)
-}
+} else if (param === 'logout') {
 
-if (param === 'logout') {
   unlinkSync(confPath)
   console.log('config file', confPath, 'removed')
+
 } else if (expiredAt < Date.now()) {
-  const now = Date.now()
-  spotify.post(`${accountURL}/token`, {
+
+  const timestamp = Date.now()
+  spotify('POST', `${accountURL}/api/token`, {
     body: { grant_type: 'refresh_token', refresh_token: conf.refresh_token },
     token: appToken,
-  }).then(async response => Promise.all([
-    execParam(response.access_token),
-    updateConf({ timestamp: now, ...response }),
+  }).then(response => Promise.all([
+    executeSpotifyCommand(response.access_token),
+    updateConf({ timestamp, ...response }),
   ])).catch(haltAndCatchFire)
+
 } else if (conf.access_token) {
-  execParam(conf.access_token).catch(haltAndCatchFire)
+
+  executeSpotifyCommand(conf.access_token)
+    .catch(haltAndCatchFire)
+
 } else {
+
   const initialState = Math.random()
     .toString(36)
     .slice(2)
 
-  const url = `https://accounts.spotify.com/authorize?${
+  const url = `${accountURL}/authorize?${
     new URLSearchParams({
-      client_id,
       redirect_uri,
       state: initialState,
+      client_id: conf.id,
       response_type: 'code',
       scope: 'user-modify-playback-state user-read-playback-state',
     })}`
 
   const openCmd = process.platform === 'linux' ? 'xdg-open' : 'open'
-  const open = spawn(openCmd, [url], { stdio: 'ignore', detached: true })
-  open.on('error', err =>
-    console.log(`Allow the use of your spotify account by opening ${url}`))
+  spawn(openCmd, [url], { stdio: 'ignore', detached: true })
+    .on('error', err =>
+      console.log(`Allow the use of your spotify account by opening ${url}`))
 
   createServer(async (req, res) => {
     try {
@@ -173,21 +173,21 @@ if (param === 'logout') {
       if (!code) throw Error('No code')
       if (state !== initialState) throw Error('State mismatch')
 
-      const now = Date.now()
+      const timestamp = Date.now()
       const body = { code, redirect_uri, grant_type: 'authorization_code' }
-      const response = await spotify
-        .post(`${accountURL}/token`, { body, token: appToken })
+      const response =
+        await spotify('POST', `${accountURL}/api/token`, { body, token: appToken })
 
       res.end(template('All Good !'))
 
       await Promise.all([
-        execParam(response.access_token),
-        updateConf({ ...conf, timestamp: now, ...response }),
+        executeSpotifyCommand(response.access_token),
+        updateConf({ timestamp, ...response }),
       ]).catch(haltAndCatchFire)
 
       process.exit(0)
     } catch (err) {
-      res.status = err.status || 500
+      res.status = err.statusCode || err.code || 500
       res.end(template(err.reason || err.statusMessage || err.message))
       setTimeout(haltAndCatchFire, 1000, err)
     }
